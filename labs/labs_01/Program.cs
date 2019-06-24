88d36de9ac0f54bfba99e860d4d469587c7f6003
Kieron Newman <KNewman@spartaglobal.com>
2019-06-24 10:28:42 +0100

    initial comment visual studio

---
+++ labs/labs_01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace labs_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! From Kieron !!!");
+
+            
+        }
+
+        
+    }
+}
